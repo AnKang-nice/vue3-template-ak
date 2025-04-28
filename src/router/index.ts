211e1dbe4dcc22cae5c7af2fce22e048b20ce1dc
haoankang <2311040049@qq.com>
2025-04-28 15:34:06 +0800

    docs: 搭建路由模版完成

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,8 @@ import { createWebHashHistory, createRouter } from 'vue-router';
 import type { RouteRecordRaw } from 'vue-router';
 import Layout from '@/layout/index.vue';
 import initRoute from './initRoute';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css'; // progress bar style
 const routes: RouteRecordRaw[] = [
 	{
 		path: '/',
@@ -20,6 +22,19 @@ const routes: RouteRecordRaw[] = [
 const router = createRouter({
 	history: createWebHashHistory(),
 	routes
+});
+
+router.beforeEach((to, from, next) => {
+	// 这里可以添加一些路由守卫的逻辑，比如权限验证等
+	NProgress.start();
+	console.log(to, from);
+	next();
+});
+
+router.afterEach((to, from) => {
+	// 这里可以添加一些路由跳转后的逻辑，比如统计等
+	console.log('afterEach', to, from);
+	NProgress.done();
 });
 
 export default router;
